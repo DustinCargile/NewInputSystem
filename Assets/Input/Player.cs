e4296f23cfa2f8fee631834318ea22f65e0067de
Dustin Cargile <dcargile84@gmail.com>
2024-01-13 01:31:39 -0600

    Completed [Bouncing Ball]

--- Assets/Input/Player.cs
+++ Assets/Input/Player.cs
@@ -1,22 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private PlayerInputActions _input;
 
+    private Rigidbody _rb;
+    private bool _isPressed = false;
+    private float _force = 0;
+
     private void Start()
     {
+        _rb = GetComponent<Rigidbody>();
         _input = new PlayerInputActions();
 
         _input.Player.Enable();
 
         _input.Player.ShootBall.performed += ShootBall_performed;
+        _input.Player.ShootBall.canceled += ShootBall_canceled;
     }
 
-    private void ShootBall_performed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void ShootBall_canceled(InputAction.CallbackContext context)
     {
-        Debug.Log("Shoot Ball..." + context);
+        if (_rb != null)
+        {
+
+            _rb.AddForce(Vector3.up * _force, ForceMode.Impulse);
+        }
+        _force = 0;
+        _isPressed = false;
     }
+
+    private void ShootBall_performed(InputAction.CallbackContext context)
+    {
+        _isPressed = true;
+
+    }
+
+    private void Update()
+    {
+
+        if (_isPressed) 
+        {
+            _force += Time.deltaTime * 10f;
+        }
+
+    }
+
+
+
+
 }
